b5cb51d5f70933f8fce82626dbce2abc6c3dbb93
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2025-03-28 20:25:47 +0500

    Python Logic Building Assignments 04

---
+++ Assignments 00 to 05/02_lists/01_add_many_number.py
@@ -0,0 +1,13 @@
+# Problem Statement
+# Write a function that takes a list of numbers and returns the sum of those numbers.
+
+
+#Solution
+#function to take a list of numbers
+def main(list_of_numbers):
+    #return the sum of the list of numbers
+    return(sum(list_of_numbers))
+
+
+if __name__ == "__main__":
+     print(main([1, 2, 3, 4, 5]))
